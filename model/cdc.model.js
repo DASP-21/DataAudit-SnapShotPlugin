9e534d87481703b2d0f0cb35c30d741fb024fb48
Akash Chouhan <akash.c1500@gmail.com>
2021-11-25 22:01:54 +0530

    init CDC DB model

--- model/cdc.model.js
+++ model/cdc.model.js
@@ -1,9 +1,30 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
+// capture format for CDC
 
+/* Note To Team:
+'content' is temporarily considered to be a String data.
+Schema for 'content' could be modified at a later stage if needed. 
+// file ->[EXTERNAL SERVICE]-> ().bin 101110101010101010010101010100101
+*/ 
 let cdcSchema = new Schema({
-    // capture format will go here.
-   
+    data_id:{
+        type: Number,
+        require: true,
+    },
+    timestamp:{
+        type: Date,
+        default: Date.now()
+    },
+    content: String,
+    change_log:[{
+        version: Number,
+        timestamp:{
+             type: Date,
+             default: Date.now()
+        },
+        content: String //FOR COMPRESSED BINARY
+    }]   
 });
 
 
