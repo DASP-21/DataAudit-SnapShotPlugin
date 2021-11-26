455108e4c0ddc7fd9396952ae8ea614708dabca6
Akash Chouhan <akash.c1500@gmail.com>
2021-11-26 22:20:31 +0530

    minor updates

--- model/cdc.model.js
+++ model/cdc.model.js
@@ -1,31 +1,36 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 // capture format for CDC
 
 /* Note To Team:
 'content' is temporarily considered to be a String data.
 Schema for 'content' could be modified at a later stage if needed. 
 // file ->[EXTERNAL SERVICE]-> ().bin 101110101010101010010101010100101
-*/ 
+*/
 let cdcSchema = new Schema({
-    data_id:{
-        type: Number,
-        require: true,
-    },
-    timestamp:{
+  data_id: {
+    type: Number,
+    require: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now(),
+  },
+  content: String,
+  change_log: [
+    {
+      version: Number,
+      timestamp: {
         type: Date,
-        default: Date.now()
+        default: Date.now(),
+      },
+      content: String, //FOR COMPRESSED BINARY
     },
-    content: String,
-    change_log:[{
-        version: Number,
-        timestamp:{
-             type: Date,
-             default: Date.now()
-        },
-        content: String //FOR COMPRESSED BINARY
-    }]   
+  ],
+  is_active: {
+    type: Boolean,
+    default: true
+  }
 });
 
-
-module.exports = mongoose.model('CDC', cdcSchema);
+module.exports = mongoose.model("CDC", cdcSchema);
--- routes/log.route.js
+++ routes/log.route.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { text } = require("express");
 const express = require("express");
 const router = express.Router();
 const CDC = require("../model/cdc.model");
@@ -30,21 +31,30 @@ router.get("/getlog/:data_id", async (req, res) => {
   }
 });
 
-// POST : /updatelog
 
+// POST : /createlog
 router.post("/createlog", async (req, res) => {
   const new_id = req.body.id;
   const newcontent = req.body.content;
-  // check
+
   const datalog = await CDC.findOne({ data_id: new_id });
-  if (datalog) {
-    const changeLog = {
-      version: new_id + (datalog.change_log.length + 1),
-      content: datalog.content,
-    };
-    datalog.change_log.push(changeLog);
-    datalog.content = newcontent;
-    datalog
+  // If the log is no longer valid, we forbid any data capture requests. [Temporary Feature]
+  if(datalog){
+    if(!datalog.is_active){
+      res.status(403).json({
+        message: `[*]Datalog for ${datalog.data_id} is no longer active.`,
+        result: `data capture is forbidden for this log.`
+      });
+    }
+    else if(datalog.is_active){
+    
+      const changeLog = {
+        version: new_id + (datalog.change_log.length + 1),
+        content: datalog.content,
+      };
+      datalog.change_log.push(changeLog);
+      datalog.content = newcontent;
+      datalog
       .save()
       .then((result) => {
         res.status(200).json({
@@ -58,7 +68,8 @@ router.post("/createlog", async (req, res) => {
           result: "error",
         });
       });
-  } else {
+  }
+}else { //DataLog deos not exist, need to create a new document.
     const newLog = new CDC({
       data_id: new_id,
       timestamp: Date.now(),
@@ -80,5 +91,43 @@ router.post("/createlog", async (req, res) => {
       });
   }
 });
+// Invalid Route:
+router.get('/*', (req,res)=>{
+  res.status(400).json({
+    message: `Invalid Get Request`,
+    result: `error`
+  });
+})
+/****************************************************************************************************************/ 
+/* Temporary Feature for our DASP Service */ 
+// Disable Data Capture for a specific data_id
+// router.get('/preventlog/:data_id', async(req,res)=>{
+//   const data_id = req.params.data_id;
+//   const log = await CDC.find({ data_id: data_id })
+//   if(log){
+//     log.is_active = !log.is_active;
+//     log
+//    .save()
+//    .then((result) => {
+//         res.status(200).json({
+//           message: `[*]Datalog #${datalog.data_id} has been disabled.`,
+//           result: "success",
+//         });
+//       })
+//     .catch((err) => {
+//         res.status(500).json({
+//           message: err.message,
+//           result: "error",
+//         });
+//     });
+//   }else{
+//     // Invalid Data_id
+//     res.status(400).json({
+//       message: `[*]Datalog #${datalog.data_id} does not exist.`,
+//       result: "failure",
+//     })
+//    }
+//   
+// })
 
 module.exports = router;
